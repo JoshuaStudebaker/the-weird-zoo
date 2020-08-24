628ee3385a1ff2ca5c281503cd20a716c3b71b48
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-08-24 16:41:17 -0600

    added method

--- app/main.js
+++ app/main.js
@@ -7,16 +7,19 @@ let baryl = new Armadillo("Beryl", "M", "Prickly Pears");
 console.log(Armadillo);
 console.log(baryl);
 
-let brownRot = new Bat("Brown Rot", "M", "Bugs", "Mosquitos", "Day", "Tree");
-console.log(brownRot);
+let bats = {
+  brownRot: new Bat("Brown Rot", "M", "Bugs", "Mosquitos", "Day", "Tree"),
+  hangrik: new Bat("Hangrik", "M", "Fruit", "Apricots", "Day", "Cave"),
+};
 
-// let critters = [
-//   Armadillo,
-//   Fox,
-//   Opossum,
-//   Porcupine,
-//   Ringtail,
-//   Stinkkatze,
-//   WashBear,
-// ];
-// let birdFowl = [Chicken, Duck, Quail, Turkey];
+function drawBatsFoods() {
+  let template = "";
+  for (let key in bats) {
+    let bfs = bats[key];
+    template += `<p>${bfs.eat(bfs.favoriteFood)}</p>`;
+  }
+
+  document.getElementById("bats").innerHTML = template;
+}
+
+drawBatsFoods();
--- app/models/Bat.js
+++ app/models/Bat.js
@@ -7,6 +7,10 @@ class Bat {
     this.sleepTime = sleepTime;
     this.home = home;
   }
+
+  eat(food) {
+    return `${this.name} the Bat loves ${food}`;
+  }
 }
 
 export default Bat;
