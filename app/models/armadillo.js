dd0a6e7c322c21addb0a6cde2cbf26c9c8ba3ad0
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-08-24 14:36:30 -0600

    start

---
+++ app/models/armadillo.js
@@ -0,0 +1,18 @@
+class Armadillo {
+  constructor(
+    name = "",
+    type = "critter",
+    gender = "",
+    color = "silver",
+    home = "burrow",
+
+    favoriteFood = ""
+  ) {
+    this.name = name;
+    this.type = type;
+    this.type = gender;
+    this.color = color;
+    this.home = home;
+    this.favoriteFood = favoriteFood;
+  }
+}
